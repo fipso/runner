fc4d6d505877383b76a312ad83f174c13b7a9918
fipso <fipso@users.noreply.github.com>
2023-11-22 10:59:11 +0100

    wip

---
+++ app.go
@@ -0,0 +1,245 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"strconv"
+
+	"github.com/docker/docker/api/types"
+	"github.com/go-git/go-git/v5"
+	cp "github.com/otiai10/copy"
+)
+
+type App struct {
+	Id               string  `json:"id"`
+	Name             string  `json:"name"`
+	ProjectName      string  `json:"project_name"`
+	ContainerId      *string `json:"container_id"`
+	BuildContainerId *string `json:"build_container_id"`
+	Port             *string `json:"port"`
+	Env              *string `json:"env"`
+	Status           string  `json:"status"`
+	GitUrl           string  `json:"git_url"`
+	GitUsername      string  `json:"git_username"`
+	GitPassword      string  `json:"git_password"`
+	RepoPath         *string `json:"src_path"`
+	TemplateId       *string `json:"template_id"`
+}
+
+func (a App) GetSlug() string {
+	return fmt.Sprintf("%s-%s", a.ProjectName, a.Name)
+}
+
+func (a App) GetDomain() string {
+	return fmt.Sprintf("%s.%s", a.GetSlug(), domain)
+}
+
+func (a App) GetUrl() string {
+	s := ""
+	if ssl {
+		s = "s"
+	}
+	return fmt.Sprintf("http%s://%s", s, a.GetDomain())
+}
+
+func (a *App) Deploy() error {
+	templateId, err := a.suggestBuildTemplate(*a.RepoPath)
+	if err != nil {
+		return err
+	}
+	a.TemplateId = ptr(templateId)
+
+	err = a.cloneRepo()
+	if err != nil {
+		return err
+	}
+
+	artifactDir, err := a.Build()
+	if err != nil {
+		return err
+	}
+
+	err = a.Run(artifactDir)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *App) Build() (artifactDir string, err error) {
+	// Create tmp mount dir
+	buildDir, err := os.MkdirTemp("./mounts/build", "")
+	if err != nil {
+		return
+	}
+	defer os.RemoveAll(buildDir)
+
+	// Copy source code into container
+	err = cp.Copy(*a.RepoPath, buildDir)
+	if err != nil {
+		return
+	}
+
+	template := deploymentTemplates[*a.TemplateId]
+
+	// Write build script into container
+	beforeScript := ""
+	if template.Build.BeforeScript != nil {
+		beforeScript = *template.Build.BeforeScript
+	}
+	afterScript := ""
+	if template.Build.AfterScript != nil {
+		afterScript = *template.Build.AfterScript
+	}
+
+	buildScript := fmt.Sprintf(
+		"#!/bin/sh\n\ncd /runner/\n\n#Before Script:\n%s\n#Run Command:\n%s\n#After Script:\n%s",
+		beforeScript,
+		template.Build.Cmd,
+		afterScript,
+	)
+	err = os.WriteFile(path.Join(buildDir, "r_build.sh"), []byte(buildScript), 0755)
+
+	// Start container
+	containerId, err := dockerRun(
+		template.Build.Image,
+		"/runner/r_build.sh",
+		nil,
+		nil,
+		nil,
+		buildDir,
+	)
+	if err != nil {
+		return
+	}
+	a.BuildContainerId = ptr(containerId)
+
+	// Watch container
+	eChan, errChan := docker.Events(context.Background(), types.EventsOptions{})
+
+LOOP:
+	for {
+		select {
+		case err := <-errChan:
+			log.Fatal(err)
+		case msg := <-eChan:
+			if msg.Type == "container" {
+				if msg.Action == "die" {
+					log.Println("Container died:", msg.Actor.ID)
+					break LOOP
+				}
+			}
+		}
+	}
+
+	// Save artifact
+	artifactDir, err = os.MkdirTemp("./artifacts", "")
+	if err != nil {
+		return
+	}
+
+	err = cp.Copy(
+		path.Join(buildDir, template.Build.Artifact),
+		path.Join(artifactDir, template.Build.Artifact),
+	)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (a *App) Run(artifactDir string) (err error) {
+	template := deploymentTemplates[*a.TemplateId]
+
+	// Select random host port for container
+	port, err := getFreePort()
+	if err != nil {
+		return
+	}
+	a.Port = ptr(strconv.Itoa(port))
+
+	// Create tmp mount dir
+	workDir, err := os.MkdirTemp("./mounts/running", "")
+	if err != nil {
+		return
+	}
+
+	// Copy artifacts into workDir
+	err = cp.Copy(artifactDir, workDir)
+	if err != nil {
+		return
+	}
+
+	// Write run script into container
+	beforeScript := ""
+	if template.Run.BeforeScript != nil {
+		beforeScript = *template.Run.BeforeScript
+	}
+	afterScript := ""
+	if template.Run.AfterScript != nil {
+		afterScript = *template.Run.AfterScript
+	}
+
+	runScript := fmt.Sprintf(
+		"#!/bin/sh\n\ncd /runner/\n\n#Before Script:\n%s\n#Run Command:\n%s\n#After Script:\n%s",
+		beforeScript,
+		template.Run.Cmd,
+		afterScript,
+	)
+	err = os.WriteFile(path.Join(workDir, "r_run.sh"), []byte(runScript), 0755)
+
+	// Start container
+	containerId, err := dockerRun(
+		template.Run.Image,
+		"/runner/r_run.sh",
+		nil,
+		ptr(template.Run.Port),
+		ptr(strconv.Itoa(port)),
+		workDir,
+	)
+	if err != nil {
+		return
+	}
+	a.ContainerId = ptr(containerId)
+
+	return
+}
+
+func (a *App) cloneRepo() error {
+	tmpDir, err := os.MkdirTemp("./repos", "")
+	if err != nil {
+		return err
+	}
+	a.RepoPath = ptr(tmpDir)
+
+	_, err = git.PlainClone(*a.RepoPath, false, &git.CloneOptions{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a App) suggestBuildTemplate(path string) (templateId string, err error) {
+	// Select deployment template based on project.json
+	var pkgJson map[string]interface{}
+	pkgJson, err = loadPackageJSON(path)
+	if err != nil {
+		return
+	}
+	deps, err := parseDependencies(pkgJson)
+	if err != nil {
+		return
+	}
+	templateId, err = findTemplateByDependencies(deps)
+	if err != nil {
+		return
+	}
+
+	return templateId, nil
+}
--- main.go
+++ main.go
@@ -1,19 +1,14 @@
 package main
 
 import (
-	"context"
+	"bytes"
 	"crypto/tls"
 	"flag"
 	"fmt"
 	"log"
-	"os"
-	"path"
-	"strconv"
 
-	"github.com/docker/docker/api/types"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/proxy"
-	cp "github.com/otiai10/copy"
 	"github.com/valyala/fasthttp"
 	"golang.org/x/crypto/acme/autocert"
 )
@@ -42,25 +37,6 @@ type StepRun struct {
 	Port string `toml:"port"`
 }
 
-type App struct {
-	Id          string `json:"id"`
-	Name        string `json:"name"`
-	ContainerId string `json:"container_id"`
-	Port        string `json:"port"`
-}
-
-func (a App) GetDomain() string {
-	return fmt.Sprintf("%s.%s", a.Name, domain)
-}
-
-func (a App) GetUrl() string {
-	s := ""
-	if ssl {
-		s = "s"
-	}
-	return fmt.Sprintf("http%s://%s.%s", s, a.Name, domain)
-}
-
 // Globals
 var deploymentTemplates map[string]TemplateConfig
 var deployments []App
@@ -82,6 +58,10 @@ func main() {
 
 	flag.Parse()
 
+	if domain == "" {
+		log.Fatal("-domain is required")
+	}
+
 	// Init
 	loadTemplates()
 	connectDocker()
@@ -96,6 +76,9 @@ func main() {
 	if err := createDirIfNotExists("./mounts/running"); err != nil {
 		log.Fatal(err)
 	}
+	if err := createDirIfNotExists("./repos"); err != nil {
+		log.Fatal(err)
+	}
 	if err := createDirIfNotExists("./artifacts"); err != nil {
 		log.Fatal(err)
 	}
@@ -106,224 +89,90 @@ func main() {
 		DisablePathNormalizing:   true,
 	})
 
-	app := fiber.New()
-	app.Get("/", func(c *fiber.Ctx) error {
+	app := fiber.New(
+		fiber.Config{
+			DisableStartupMessage: true,
+		},
+	)
+
+	app.Use(func(c *fiber.Ctx) error {
+		// Skip API routes
+		if bytes.HasPrefix(c.Request().URI().Path(), []byte("/api")) {
+			return c.Next()
+		}
+
+		// Forward to docker container
 		app := getAppByDomain(c.Hostname())
 		if app == nil {
 			return fiber.NewError(fiber.StatusNotFound, "App not found")
 		}
 
-		return proxy.Do(c, fmt.Sprintf("http://127.0.0.1:%s", app.Port))
+		err := proxy.Do(c, fmt.Sprintf("http://127.0.0.1:%s", *app.Port))
+		if err != nil {
+			return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+		}
+
+		return c.Next()
+	})
+
+	app.Post("/api/deploy", func(c *fiber.Ctx) error {
+		var body struct {
+			ProjectName string `json:"project_name"`
+			GitUrl      string `json:"git_url"`
+			GitUsername string `json:"git_username"`
+			GitPassword string `json:"git_password"`
+			Env         string `json:"env"`
+		}
+
+		if err := c.BodyParser(&body); err != nil {
+			return fiber.NewError(fiber.StatusBadRequest, err.Error())
+		}
+
+		app := App{
+			ProjectName: body.ProjectName,
+			GitUrl:      body.GitUrl,
+			GitUsername: body.GitUsername,
+			GitPassword: body.GitPassword,
+			Env:         ptr(body.Env),
+		}
+
+		return app.Deploy()
 	})
 
 	if ssl {
-		// Certificate manager
-		m := &autocert.Manager{
-			Prompt: autocert.AcceptTOS,
-			// Replace with your domain
-			HostPolicy: autocert.HostWhitelist(fmt.Sprintf("*.%s", domain)),
-			// Folder to store the certificates
-			Cache: autocert.DirCache("./certs"),
-		}
-
-		// TLS Config
-		cfg := &tls.Config{
-			// Get Certificate from Let's Encrypt
-			GetCertificate: m.GetCertificate,
-			// By default NextProtos contains the "h2"
-			// This has to be removed since Fasthttp does not support HTTP/2
-			// Or it will cause a flood of PRI method logs
-			// http://webconcepts.info/concepts/http-method/PRI
-			NextProtos: []string{
-				"http/1.1", "acme-tls/1",
-			},
-		}
-		ln, err := tls.Listen("tcp", fmt.Sprintf(":%s", sslPort), cfg)
-		if err != nil {
-			panic(err)
-		}
-
-		// Start server
-		log.Fatal(app.Listener(ln))
-	} else {
-		log.Fatal(app.Listen(fmt.Sprintf(":%s", port)))
-	}
-
-}
-
-func deployApp(path string, env string) {
-	_, templateId, artifactDir, err := buildApp(path, env)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	contaierId, port, err := runApp(templateId, artifactDir)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println(contaierId, port)
-}
-
-func buildApp(
-	srcPath, env string,
-) (containerId string, templateId string, artifactDir string, err error) {
-	// Select deployment template based on project.json
-	var pkgJson map[string]interface{}
-	pkgJson, err = loadPackageJSON(srcPath)
-	if err != nil {
-		return
-	}
-	deps, err := parseDependencies(pkgJson)
-	if err != nil {
-		return
-	}
-	templateId, err = findTemplateByDependencies(deps)
-	if err != nil {
-		return
-	}
-	template := deploymentTemplates[templateId]
-
-	// Create tmp mount dir
-	buildDir, err := os.MkdirTemp("./mounts/build", "")
-	if err != nil {
-		return
-	}
-	defer os.RemoveAll(buildDir)
-
-	log.Println("Build Dir Mount:", buildDir)
-
-	// Copy source code into container
-	err = cp.Copy(srcPath, buildDir)
-	if err != nil {
-		return
-	}
-
-	// Write build script into container
-	beforeScript := ""
-	if template.Build.BeforeScript != nil {
-		beforeScript = *template.Build.BeforeScript
-	}
-	afterScript := ""
-	if template.Build.AfterScript != nil {
-		afterScript = *template.Build.AfterScript
-	}
-
-	buildScript := fmt.Sprintf(
-		"#!/bin/sh\n\ncd /runner/\n\n#Before Script:\n%s\n#Run Command:\n%s\n#After Script:\n%s",
-		beforeScript,
-		template.Build.Cmd,
-		afterScript,
-	)
-	err = os.WriteFile(path.Join(buildDir, "r_build.sh"), []byte(buildScript), 0755)
-
-	// Start container
-	containerId, err = dockerRun(
-		template.Build.Image,
-		"/runner/r_build.sh",
-		nil,
-		nil,
-		nil,
-		buildDir,
-	)
-	if err != nil {
-		return
-	}
-
-	// Watch container
-	eChan, errChan := docker.Events(context.Background(), types.EventsOptions{})
-
-LOOP:
-	for {
-		select {
-		case err := <-errChan:
-			log.Fatal(err)
-		case msg := <-eChan:
-			if msg.Type == "container" {
-				if msg.Action == "die" {
-					log.Println("Container died:", msg.Actor.ID)
-					break LOOP
-				}
+		go func() {
+			// Certificate manager
+			m := &autocert.Manager{
+				Prompt: autocert.AcceptTOS,
+				// Replace with your domain
+				HostPolicy: autocert.HostWhitelist(fmt.Sprintf("*.%s", domain)),
+				// Folder to store the certificates
+				Cache: autocert.DirCache("./certs"),
 			}
-		}
+
+			// TLS Config
+			cfg := &tls.Config{
+				// Get Certificate from Let's Encrypt
+				GetCertificate: m.GetCertificate,
+				// By default NextProtos contains the "h2"
+				// This has to be removed since Fasthttp does not support HTTP/2
+				// Or it will cause a flood of PRI method logs
+				// http://webconcepts.info/concepts/http-method/PRI
+				NextProtos: []string{
+					"http/1.1", "acme-tls/1",
+				},
+			}
+			ln, err := tls.Listen("tcp", fmt.Sprintf(":%s", sslPort), cfg)
+			if err != nil {
+				panic(err)
+			}
+
+			// Start server
+			log.Println("HTTPS Listening on port:", sslPort)
+			log.Fatal(app.Listener(ln))
+		}()
 	}
 
-	// Log
-	log.Println(dockerLogs(containerId))
-
-	// Save artifact
-	artifactDir, err = os.MkdirTemp("./artifacts", "")
-	if err != nil {
-		return
-	}
-
-	log.Println("Artifact Dir:", artifactDir)
-
-	err = cp.Copy(
-		path.Join(buildDir, template.Build.Artifact),
-		path.Join(artifactDir, template.Build.Artifact),
-	)
-	if err != nil {
-		return
-	}
-
-	return
-}
-
-func runApp(templateId, artifactDir string) (containerId string, port int, err error) {
-	template := deploymentTemplates[templateId]
-
-	// Select random host port for container
-	port, err = getFreePort()
-	if err != nil {
-		return
-	}
-	log.Println("Random port", port)
-
-	// Create tmp mount dir
-	workDir, err := os.MkdirTemp("./mounts/running", "")
-	if err != nil {
-		return
-	}
-
-	log.Println("Run Dir Mount:", workDir)
-
-	// Copy artifacts into workDir
-	err = cp.Copy(artifactDir, workDir)
-	if err != nil {
-		return
-	}
-
-	// Write run script into container
-	beforeScript := ""
-	if template.Run.BeforeScript != nil {
-		beforeScript = *template.Run.BeforeScript
-	}
-	afterScript := ""
-	if template.Run.AfterScript != nil {
-		afterScript = *template.Run.AfterScript
-	}
-
-	runScript := fmt.Sprintf(
-		"#!/bin/sh\n\ncd /runner/\n\n#Before Script:\n%s\n#Run Command:\n%s\n#After Script:\n%s",
-		beforeScript,
-		template.Run.Cmd,
-		afterScript,
-	)
-	err = os.WriteFile(path.Join(workDir, "r_run.sh"), []byte(runScript), 0755)
-
-	// Start container
-	containerId, err = dockerRun(
-		template.Run.Image,
-		"/runner/r_run.sh",
-		nil,
-		ptr(template.Run.Port),
-		ptr(strconv.Itoa(port)),
-		workDir,
-	)
-	if err != nil {
-		return
-	}
-
-	return
+	log.Println("HTTP Listening on port:", port)
+	log.Fatal(app.Listen(fmt.Sprintf(":%s", port)))
 }
